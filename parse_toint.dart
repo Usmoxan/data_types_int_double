1de6f408beb8ee9d3f8dc18d2fc48aacf085dffd
Usmoxan <101461756+Usmoxan@users.noreply.github.com>
2022-12-15 16:46:26 +0500

    Create parse_toint.dart

---
+++ parse_toint.dart
@@ -0,0 +1,11 @@
+void main(){
+  //int
+  int num1 = 12;
+  //double
+  double num2 = 12.12;
+//parse String to int
+  String number =  "44";
+
+  int parsedNumber = int.parse(number);
+  print(parsedNumber);
+}
